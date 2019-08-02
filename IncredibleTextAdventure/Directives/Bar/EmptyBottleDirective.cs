fe04c396b22f48cf1786de3fa474228b3f119dd4
jfont <jfont@LAPTOP-I73V7O29>
2019-08-02 14:54:00 +0200

    Forgat a condition ;)

--- IncredibleTextAdventure/Directives/Bar/EmptyBottleDirective.cs
+++ IncredibleTextAdventure/Directives/Bar/EmptyBottleDirective.cs
@@ -23,12 +23,19 @@ namespace IncredibleTextAdventure.Directives.Bar
 
         public void TryApply(string cmd, GameContext context)
         {
-            _consoleWriter.WriteToConsole("You empty the [bottle] in a sink. The dense and opaque liquid falls slowly out of the bottle. " +
-                                          "The air doesn't smell that bad. After a few seconds, the liquid start to flow even faster. " +
-                                          "It comes out as a single 'block' of dense matter, and disappear into the sink. You put the " +
-                                          "[empty bottle] on the bar, and take a few seconds to realize that a [golden key] fell from " +
-                                          "inside the bottle. ");
-            context.TriggerSpecialEvent(Constants.Events.EmptyBottle);
+            if (context.GetCurrentRoom().IsItemInRoom(Constants.Items.Bottle))
+            {
+                _consoleWriter.WriteToConsole("You empty the [bottle] in a sink. The dense and opaque liquid falls slowly out of the bottle. " +
+                                              "The air doesn't smell that bad. After a few seconds, the liquid start to flow even faster. " +
+                                              "It comes out as a single 'block' of dense matter, and disappear into the sink. You put the " +
+                                              "[empty bottle] on the bar, and take a few seconds to realize that a [golden key] fell from " +
+                                              "inside the bottle. ");
+                context.TriggerSpecialEvent(Constants.Events.EmptyBottle);
+            }
+            else
+            {
+                _consoleWriter.WriteToConsole("You already emptied the bottle !");
+            }
         }
     }
 }

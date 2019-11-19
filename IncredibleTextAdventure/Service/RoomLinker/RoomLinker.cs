83aa079f0472418ec14e2ec7563353e68cad2ea7
Julien FONTANA <jfont@LAPTOP-I73V7O29>
2019-11-19 12:55:37 +0100

    If it's ugly but if it works...

--- IncredibleTextAdventure/Service/RoomLinker/RoomLinker.cs
+++ IncredibleTextAdventure/Service/RoomLinker/RoomLinker.cs
@@ -1,93 +1,26 @@
-﻿using IncredibleTextAdventure.Constant;
-using IncredibleTextAdventure.Rooms;
+﻿using IncredibleTextAdventure.Rooms;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using IncredibleTextAdventure.Service.LanguageModule;
 
 namespace IncredibleTextAdventure.Service.RoomLinker
 {
     public class RoomLinker : IRoomLinker
     {
+        private readonly ILanguageConst _languageConst;
         private readonly List<IRoom> _rooms;
-        private readonly Dictionary<string, List<string>> _linkedRoomList = new Dictionary<string, List<string>>
-        {
-            {
-                Constants.Rooms.OpenSpace, new List<string>
-                {
-                    Constants.Rooms.DeskOne,
-                    Constants.Rooms.DeskTwo,
-                    Constants.Rooms.DeskThree,
-                    Constants.Rooms.DeskFour,
-                    Constants.Rooms.ServerRoom,
-                    Constants.Rooms.RestingRoom
-                }
-            },
-            {
-                Constants.Rooms.DeskOne, new List<string>
-                {
-                    Constants.Rooms.OpenSpace,
-                    Constants.Rooms.DeskTwo,
-                    Constants.Rooms.DeskThree,
-                    Constants.Rooms.DeskFour
-                }
-            },
-            {
-                Constants.Rooms.DeskTwo, new List<string>
-                {
-                    Constants.Rooms.OpenSpace,
-                    Constants.Rooms.DeskOne,
-                    Constants.Rooms.DeskThree,
-                    Constants.Rooms.DeskFour
-                }
-            },
-            {
-                Constants.Rooms.DeskThree, new List<string>
-                {
-                    Constants.Rooms.OpenSpace,
-                    Constants.Rooms.DeskOne,
-                    Constants.Rooms.DeskTwo,
-                    Constants.Rooms.DeskFour
-                }
-            },
-            {
-                Constants.Rooms.DeskFour, new List<string>
-                {
-                    Constants.Rooms.OpenSpace,
-                    Constants.Rooms.DeskOne,
-                    Constants.Rooms.DeskTwo,
-                    Constants.Rooms.DeskThree,
-                    Constants.Rooms.DeskFour
-                }
-            },
-            {
-                Constants.Rooms.RestingRoom, new List<string>
-                {
-                    Constants.Rooms.OpenSpace
-                }
-            },
-            {
-                Constants.Rooms.ServerRoom, new List<string>
-                {
-                    Constants.Rooms.ComputerRoom,
-                    Constants.Rooms.OpenSpace
-                }
-            },
+        private Dictionary<string, List<string>> _linkedRoomList;
 
-            {
-                Constants.Rooms.ComputerRoom, new List<string>
-                {
-                    Constants.Rooms.ServerRoom
-                }
-            }
-        };
-
-        public RoomLinker(IEnumerable<IRoom> rooms)
+        public RoomLinker(IEnumerable<IRoom> rooms, ILanguageConst languageConst)
         {
+            _languageConst = languageConst;
             _rooms = rooms.ToList();
         }
 
         public void InitRoomConnection()
         {
+            InitRoomLink();
             foreach (var roomName in _linkedRoomList.Keys)
             {
                 var room = _rooms.FirstOrDefault(r => r.Name.EqualsIgnoreCase(roomName));
@@ -106,5 +39,81 @@ namespace IncredibleTextAdventure.Service.RoomLinker
                 room.SetLinkedRoom(linkedRooms);
             }
         }
+
+        private void InitRoomLink()
+        {
+            _linkedRoomList = new Dictionary<string, List<string>>
+            {
+                {
+                    _languageConst.OpenSpaceName, new List<string>
+                    {
+                        _languageConst.DeskOneName,
+                        _languageConst.DeskTwoName,
+                        _languageConst.DeskThreeName,
+                        _languageConst.DeskFourName,
+                        _languageConst.ServerRoomName,
+                        _languageConst.RestingRoomName
+                    }
+                },
+                {
+                    _languageConst.DeskOneName, new List<string>
+                    {
+                        _languageConst.OpenSpaceName,
+                        _languageConst.DeskTwoName,
+                        _languageConst.DeskThreeName,
+                        _languageConst.DeskFourName
+                    }
+                },
+                {
+                    _languageConst.DeskTwoName, new List<string>
+                    {
+                        _languageConst.OpenSpaceName,
+                        _languageConst.DeskOneName,
+                        _languageConst.DeskThreeName,
+                        _languageConst.DeskFourName
+                    }
+                },
+                {
+                    _languageConst.DeskThreeName, new List<string>
+                    {
+                        _languageConst.OpenSpaceName,
+                        _languageConst.DeskOneName,
+                        _languageConst.DeskTwoName,
+                        _languageConst.DeskFourName
+                    }
+                },
+                {
+                    _languageConst.DeskFourName, new List<string>
+                    {
+                        _languageConst.OpenSpaceName,
+                        _languageConst.DeskOneName,
+                        _languageConst.DeskTwoName,
+                        _languageConst.DeskThreeName,
+                        _languageConst.DeskFourName
+                    }
+                },
+                {
+                    _languageConst.RestingRoomName, new List<string>
+                    {
+                        _languageConst.OpenSpaceName
+                    }
+                },
+                {
+                    _languageConst.ServerRoomName, new List<string>
+                    {
+                        _languageConst.ComputerRoomName,
+                        _languageConst.OpenSpaceName
+                    }
+                },
+
+                {
+                    _languageConst.ComputerRoomName, new List<string>
+                    {
+                        _languageConst.ServerRoomName
+                    }
+                }
+            };
+        }
+
     }
 }
